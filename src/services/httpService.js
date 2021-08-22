aa9c85ad1c05c57364da9c98a6e4a30ac2bf385f
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-08-23 03:00:47 +0700

    update httpServices:

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import envConfig from '../../env-config';
-import hostname from './hostnameServerApi';
+import { hostname, port } from './hostnameServerApi';
 
 const api = axios.create({
-  baseURL: `http://${hostname}:${envConfig.LOCAL_API_PORT}/${envConfig.LOCAL_API_VERSION}`,
+  baseURL: `http://${hostname}:${port}/${envConfig.LOCAL_API_VERSION}`,
   headers: {
     'content-type': 'application/json',
   },
